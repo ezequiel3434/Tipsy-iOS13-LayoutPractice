7622bffbb617f290a9f6afd3ca8610802da32893
ezequiel3434 <ezequiel3434@gmail.com>
2020-09-18 15:14:23 -0300

    result vc connected ui

---
+++ Tipsy/Controllers/ResultViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ResultViewController.swift
+//  Tipsy
+//
+//  Created by Ezequiel Parada Beltran on 18/09/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+   
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+
+}
