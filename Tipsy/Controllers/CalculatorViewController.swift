901d1c393c7984f92516213930084f562232a93a
ezequiel3434 <ezequiel3434@gmail.com>
2020-09-18 16:34:39 -0300

    connecting butns with the logic

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -15,15 +15,31 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    var pct=Float()
+    var numOfPeaple = 0
     
    
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        let pctButtons = [zeroPctButton,tenPctButton,twentyPctButton]
+        sender.isSelected = true
+        for item in pctButtons {
+            if item != sender {
+                item?.isSelected = false
+                
+            }
+        }
+        pct = Float(String(describing: sender.titleLabel!.text!.replacingOccurrences(of: "%", with: "")))!/100
+        
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        numOfPeaple = Int(sender.value)
+        splitNumberLabel.text = String(numOfPeaple)
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print("\(pct) was selected")
+        print(numOfPeaple)
     }
 }
 
---
+++ Tipsy/Resources/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Tipsy
+//
+//  Created by Ezequiel Parada Beltran on 18/09/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+extension CalculatorViewController {
+    
+}
